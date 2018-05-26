762792a7a077637e8d14792ce40126c5c07fd08b
bigdog0425 <39252443+bigdog0425@users.noreply.github.com>
2018-05-26 16:01:36 +0800

    Update post_php.php

--- post_php.php
+++ post_php.php
@@ -2,7 +2,7 @@
 //print_r($_POST);
 $dsn="mysql:host=192.168.32.90;dbname=dbname01";
 $db=new PDO($dsn,"dbname01","dbname@pwd");
-$rs= $db->query("select * FROM users WHERE account = '".$_POST['account']."' and password='".$_POST['password']."'");
+$rs= $db->query("select * FROM users WHERE account = '$_POST[account]' and password='$_POST[password]'");
 $result=$rs->fetch();
 
 //print_r($result);
