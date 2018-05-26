59768938b8e067db9135af606f81583fa0ca6d2b
bigdog0425 <bigdog@hcvs.kh.edu.tw>
2018-05-26 15:56:06 +0800

    登入帳號檢查

--- post_php.php
+++ post_php.php
@@ -1,3 +1,10 @@
 <?php
+//print_r($_POST);
+$dsn="mysql:host=192.168.32.90;dbname=dbname01";
+$db=new PDO($dsn,"dbname01","dbname@pwd");
+$rs= $db->query("select * FROM users WHERE account = '".$_POST['account']."' and password='".$_POST['password']."'");
+$result=$rs->fetch();
 
-print_r($_POST);
+//print_r($result);
+if (!$result ) echo "找不到" ;
+else echo "登入成功";
