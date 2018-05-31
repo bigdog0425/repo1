64909932c8e8d78eecfdeda36d5f137564eee9ab
bigdog0425 <39252443+bigdog0425@users.noreply.github.com>
2018-05-31 16:15:16 +0800

    Delete quert.php

--- quert.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dsn="mysql:host=192.168.32.90;dbname=dbname01";
-$db=new PDO($dsn,"dbname01","dbname@pwd");
-$rs= $db->query("select account FROM users WHERE points = 0");
-
-$result=$rs->fetch();
-print_r($result);
-?>
